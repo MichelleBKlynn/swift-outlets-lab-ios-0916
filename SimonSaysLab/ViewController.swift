235f32917fd046a2c31ed4f49021be4333ae36bc
MichelleBKlynn <michellestaton@gmail.com>
2016-09-08 17:56:56 -0400

    Complete answers

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -5,6 +5,13 @@
 //  Created by James Campagno on 5/31/16.
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
+//@IBAction func dieButtonTapped(sender: AnyObject) {
+//let diceRoll = randomDiceRoll()
+//switch diceRoll {
+//case 1:
+
+//
+//
 
 import UIKit
 
@@ -13,13 +20,47 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
+
+    
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
+    @IBAction func redButtonTapped(sender: AnyObject) {
+        simonSaysGame.guessRed()
+        print("Red button tapped")
+        buttonsClicked += 1
+        updateWinLabel()
+    }
+    
+    @IBAction func greenButtonTapped(sender: AnyObject) {
+        simonSaysGame.guessGreen()
+        print("Green button tapped")
+        buttonsClicked += 1
+        updateWinLabel()
+    }
+    
+    @IBAction func yellowButtonTapped(sender: AnyObject) {
+        simonSaysGame.guessYellow()
+        print("Yellow button tapped")
+        buttonsClicked += 1
+        updateWinLabel()
+    }
+    
+    @IBAction func blueButtonTapped(sender: AnyObject) {
+        simonSaysGame.guessBlue()
+        print("Blue button tapped")
+        buttonsClicked += 1
+        updateWinLabel()
+    }
+    
     override func viewDidLoad() {
+        winLabel.hidden = true
+        
         super.viewDidLoad()
     }
 }
+
+
 
 // MARK: - SimonSays Game Methods
 extension ViewController {
@@ -28,7 +69,6 @@ extension ViewController {
         UIView.transitionWithView(startGameButton, duration: 0.9, options: .TransitionFlipFromBottom , animations: {
             self.startGameButton.hidden = true
             }, completion: nil)
-        
         displayTheColors()
     }
     
@@ -46,5 +86,50 @@ extension ViewController {
                     print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
                 }
         })
+    
     }
+    
+    func updateWinLabel() {
+        if buttonsClicked == 5 && simonSaysGame.wonGame() {
+            winLabel.hidden = false
+            print("You won!")
+            winLabel.text = "You won" }
+        
+        else if buttonsClicked == 5 && !simonSaysGame.wonGame() {
+            winLabel.hidden = false
+            print("Nope, try again.")
+            winLabel.text = "Nope try again." }
+        
+        
+        
+    }
+    
+
+
 }
+
+
+//
+//func updateWinLabel() {
+//    switch buttonsClicked == 5 {
+//    case simonSaysGame.wonGame():
+//        winLabel.hidden = false
+//        print("You won!")
+//        winLabel.text = "You won"
+//        
+//    case !simonSaysGame.wonGame():
+//        winLabel.hidden = false
+//        print("Nope, try again.")
+//        winLabel.text = "Nope try again."
+//        
+//    default:
+//        winLabel.hidden = false
+//        
+//        
+//}
+
+
+
+
+
+
